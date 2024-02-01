12160fa2d9b9238ca46cd07a090782446e743cbd
Ricardojrn <cadonascimentosilva@hotmail.com>
2024-02-01 19:47:00 -0300

    first commit

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -50,5 +52,12 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("tamanho da lista "+listaDeFilmes.size());
+        System.out.println("primeiro filme "+listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
     }
 }
